01bece97a0eaf0b777edb89f74401163c0f8e8df
r-bjorn <ravidtrivedi111@gmail.com>
2021-11-17 21:22:54 -0500

    Adding Player
    Adding a player with 2 Scripts for movement and camera follow

---
+++ COMP-3770-A4/Assets/Scripts/MovePlayer.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePlayer : MonoBehaviour
+{
+
+    public CharacterController controller;
+
+    public float speed = 12f;
+    public float gravity = -9.81f;
+    public float jumpHeight = 1f;
+
+    public Transform groundCheck;
+    public float groundDist = 0.4f;
+    public LayerMask groundMask;
+
+    private Vector3 velocity;
+    private bool isGrounded;
+    
+
+    void Update()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDist, groundMask);
+
+        if (isGrounded && velocity.y < 0)
+            velocity.y = -2f;
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        controller.Move(move * speed * Time.deltaTime);
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+        
+        velocity.y += gravity * Time.deltaTime;
+
+        controller.Move(velocity * Time.deltaTime);
+    }
+}
